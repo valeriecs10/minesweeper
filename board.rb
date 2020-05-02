f5e2e1bb9a7bd3d81de633d6a24757882db2d535
Valerie Campbell <valeriecs10@gmail.com>
2020-05-01 21:53:08 -0700

    add won? method to board class and initialize method to minesweeper class

--- board.rb
+++ board.rb
@@ -15,4 +15,10 @@ class Board
     def empty_grid(size)
         Array.new(size) { Array.new(size) { Tile.new(self) } }
     end
+
+    def won?
+        @grid.all? do |row| 
+            row.all? { |tile| tile.hidden == false || tile.bomb == true }
+        end
+    end
 end
---
+++ minesweeper.rb
@@ -0,0 +1,6 @@
+class Minesweeper
+    def initialize(size = 9)
+        @board = Board.new(size)
+    end
+
+end
