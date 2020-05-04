b04e6df4b5640bac556b6d0020b8147b1618c58d
Valerie Campbell <valeriecs10@gmail.com>
2020-05-03 17:27:36 -0700

    refactor flag_mode method in minesweeper class

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,7 +3,6 @@ require 'byebug'
 
 class Minesweeper
 
-    attr_reader :board # DELETE AFTER TESTING
     def initialize(size = 9)
         @board = Board.new(size)
         @bombed = false
@@ -17,51 +16,54 @@ class Minesweeper
             if @bombed == true
                 puts
                 puts "You hit a bomb! You lose!!"
-                break
             end
         end
-        refresh_board
-        puts "Congratulations! You cleared the board!" if @board.won?
+
+        if @board.won?
+            refresh_board
+            puts "Congratulations! You cleared the board!"
+        end
     end
 
     def flag_mode
         @flag_mode = true
         while @flag_mode == true
             refresh_board
-            puts
-            puts "FLAG MODE (enter 'f' again to exit flag mode)"
-            puts "Which hidden tile would you like to flag or unflag?"
-            puts
+            flag_prompt
+            
             input = gets.chomp
-
             if input.downcase == 'f'
                 @flag_mode = false
                 next
-            end
-
-            flag_pos = parse_pos(input)
-
-            if @board.valid_pos?(flag_pos) && @board[flag_pos].hidden == true
-                @board[flag_pos].toggle_flag
             else
-                puts "Must be a valid, hidden tile. Try again."
+                flag_pos = parse_pos(input)
             end
+            
+            toggle_flag_if_valid(flag_pos)
         end
     end
-
+    
+    def toggle_flag_if_valid(flag_pos)
+        if @board.valid_pos?(flag_pos) && @board[flag_pos].hidden == true
+            @board[flag_pos].toggle_flag
+        else
+            puts "Must be a valid, hidden tile. Try again."
+        end
+    end
+    
     def get_pos
         pos = nil
-
+        
         until pos && @board.valid_pos?(pos)
             prompt
             input = gets.chomp
-
+            
             if input.downcase == 'f'
                 flag_mode
                 refresh_board
                 next
             end
-
+            
             begin
                 pos = parse_pos(input)
             rescue => error
@@ -71,7 +73,7 @@ class Minesweeper
                 
                 pos = nil
             end
-
+            
             if !@board.valid_pos?(pos) || @board[pos].is_flagged?
                 puts "You can only reveal a hidden, unflagged tile on the board. Try again."
                 pos = nil
@@ -79,12 +81,12 @@ class Minesweeper
         end
         pos
     end
-
+    
     def refresh_board
         system("clear")
         @board.render
     end
-
+    
     def reveal_tiles(pos)
         if @board[pos].neighbor_bomb_count == 0
             @board[pos].neighbors.each do |n|
@@ -95,22 +97,29 @@ class Minesweeper
             end
         end
     end
-
+    
     def play_turn
         guess_pos = get_pos
         @board[guess_pos].reveal
-
+        
         if @board[guess_pos].is_bomb?
             @bombed = true
         else
             reveal_tiles(guess_pos) 
         end
     end
-
+    
     def prompt
         puts
         puts "Enter 'f' to switch to flag mode or enter the"
         puts "position of the tile that you want to reveal: "
+        puts
+    end
+    
+    def flag_prompt
+        puts
+        puts "FLAG MODE (enter 'f' again to exit flag mode)"
+        puts "Which hidden tile would you like to flag or unflag?"
         puts
     end
 
